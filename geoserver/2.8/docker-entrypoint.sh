14d76c85f44697463c75bf29bbdda7fa73832fc5
Jonathan Meyer <jon@gisjedi.com>
2016-06-02 17:56:26 -0400

    Adjusted heap allocation

--- geoserver/2.8/docker-entrypoint.sh
+++ geoserver/2.8/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-HEAP_SIZE=`awk 'BEGIN{printf "%.0f\n", '${INSTANCE_MEMORY}'*(3/4)}'`
+HEAP_SIZE=`awk 'BEGIN{printf "%.0f\n", '${INSTANCE_MEMORY}'*(1/2)}'`
 export CATALINA_OPTS="${CATALINA_OPTS} -Xms${HEAP_SIZE}m -Xmx${HEAP_SIZE}m"
 if [ "$OOM_KILL" = true ] ; then
     echo 'Enabling OutOfMemory exception killer'
